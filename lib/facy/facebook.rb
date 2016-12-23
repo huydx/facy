087b3d1e4b1218262f4d98c69a73c0985b13a051
Orson Teodoro <orsonteodoro@yahoo.com>
2016-12-22 22:13:59 -0800

    Use the new converter instead of the old one that made use of the deprecated Graph API edges for notifications and home.

--- lib/facy/facebook.rb
+++ lib/facy/facebook.rb
@@ -13,6 +13,7 @@ module Facy
     rescue Koala::Facebook::ServerError
       retry_wait
     rescue Koala::Facebook::APIError
+      puts "failed facebook_me"
       expired_session
     rescue Exception => e
       error e
@@ -25,12 +26,35 @@ module Facy
     #RULE: all facebook method should be prefix with facebook
     def facebook_stream_fetch
       return unless facebook_status == ConnectionStatus::NORMAL
-      streams = @graph.get_connections("me", "home")
-      streams.each { |post| stream_print_queue << graph2item(post) }
+      #streams = @graph.get_connections("me", "feed") #home
+      #streams.each { |post| stream_print_queue << graph2item(post) }
+      scwd=File.dirname(__FILE__)
+      streams_options=""
+      if config[:show_latest_newsfeed]
+		streams_options+="--show-latest "
+	  end
+	  if config[:id_timestamp_last_news_feed] != 0
+		streams_options+="--newer-than=#{config[:id_timestamp_last_news_feed]} " #reduce data transmission/server load
+	  end
+	  #--web-security=no is for ajax facebook videos. it has to connect to cdn
+      streams = `casperjs --ssl-protocol=tlsv1 --web-security=no --prefs-folder="#{config[:session_file_folder]}" --user-agent="#{config[:user_agent]}" --cookies-file="#{config[:fb_cookiejar]}" --num-result-pages=#{config[:news_feed_pages]} #{streams_options} #{scwd}/facebook-news-feed.js`
+      if streams.length <= 0
+        return
+      end
+      streams = JSON.parse(streams)
+      streams.each do |post|
+          if post["id"].to_i > config[:id_timestamp_last_news_feed]
+            config[:id_timestamp_last_news_feed] = post["id"].to_i
+          end
+          #puts "post: "+post.inspect
+          stream_print_queue << www2item(post)
+      end
       log(:info, "fetch stream ok")
     rescue Koala::Facebook::ServerError
       retry_wait
-    rescue Koala::Facebook::APIError
+    rescue Koala::Facebook::APIError => e
+      puts "failed facebook_stream_fetch"
+      error e.message
       expired_session
     rescue Exception => e
       error e
@@ -38,13 +62,34 @@ module Facy
 
     def facebook_notification_fetch
       return unless facebook_status == ConnectionStatus::NORMAL
-      notifications = @graph.get_connections("me", "notifications")
-      notifications.each { |notifi| notification_print_queue << graph2item(notifi) }
+      #notifications = @graph.get_connections("me", "notifications") #deprecated
+      #notifications.each { |notifi| notification_print_queue << graph2item(notifi) }
+      scwd=File.dirname(__FILE__)
+      notification_options = %Q[--ssl-protocol=tlsv1 --user-agent="#{config[:user_agent]}" --cookies-file="#{config[:fb_cookiejar]}" --num-result-pages=#{config[:notification_pages]}]
+      if config[:unread_notifications_only]
+        notification_options += "--unread-only "
+      end
+      if config[:id_timestamp_last_notifications] != 0
+        notification_options += "--newer-than=#{config[:id_timestamp_last_notifications]} " #reduce data transfer/server load
+      end
+      notifications = `casperjs #{notification_options} #{scwd}/facebook-notifications.js`
+      if notifications.length <= 0
+        return
+      end
+      notifications = JSON.parse(notifications)
+      notifications.each do |notifi|
+          if notifi["date"].to_i > config[:id_timestamp_last_notifications]
+			config[:id_timestamp_last_notifications] = notifi["date"].to_i
+          end
+          notification_print_queue << www2item(notifi)
+      end
       log(:info, "fetch notification ok")
     rescue Koala::Facebook::ServerError
       retry_wait
-    rescue Koala::Facebook::APIError
-      expired_session
+    rescue Koala::Facebook::APIError => e
+      puts "failed facebook_notification_fetch"
+      error e.message
+      #expired_session
     rescue Exception => e
       error e
     end
@@ -54,6 +99,7 @@ module Facy
     rescue Koala::Facebook::ServerError
       retry_wait
     rescue Koala::Facebook::APIError
+      puts "failed facebook_post"
       expired_session
     rescue Exception => e
       error e
@@ -65,6 +111,7 @@ module Facy
     rescue Koala::Facebook::ServerError
       retry_wait
     rescue Koala::Facebook::APIError
+      puts "failed facebook_like"
       expired_session
     rescue Exception => e
       error e
@@ -75,6 +122,7 @@ module Facy
     rescue Koala::Facebook::ServerError => e
       retry_wait
     rescue Koala::Facebook::APIError => e
+      puts "failed facebook_set_seen"
       error e.message
       expired_session
     rescue Exception => e
@@ -82,12 +130,13 @@ module Facy
     end
 
     def facebook_mailbox
-      @graph.get_connections("me", "inbox")
+      @graph.get_connections("me", "inbox") #deprecated
     rescue Koala::Facebook::ServerError => e
       retry_wait
     rescue Koala::Facebook::APIError => e
+      puts "failed facebook_mailbox"
       error e.message
-      expired_session
+      #expired_session
     rescue Exception => e
       error e
     end
@@ -98,6 +147,7 @@ module Facy
     rescue Koala::Facebook::ServerError
       retry_wait
     rescue Koala::Facebook::APIError
+      puts "failed facebook_comment"
       expired_session
     rescue Exception => e
       error e
@@ -120,6 +170,7 @@ module Facy
     def login
       token = config[:access_token]
       @graph = Koala::Facebook::API.new(token)
+      Koala.config.api_version = "v#{config[:facebook_api_version]}"
       log(:info, "login ok at facebook module: #{@graph}")
     end
   end
